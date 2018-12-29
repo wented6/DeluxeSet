e182575e77f1d176665e33fba45e356c808f4284
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-29 04:24:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,48 +14,41 @@ const p5 = new D5.Client();
 
 
     console.log("oNNN1");
-    p1.on('ready', () => {
-        client.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
-    
-    });
+p1.on('guildMemberAdd', member => {
+    p1.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
+
+});
 
 
 
     console.log("oNNN2");
-    p2.on('ready', () => {
-        client.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
-    
-    });
+p2.on('guildMemberAdd', member => {
+    p2.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
+
+});
+
 
 
       console.log("oNNN3");
-    p3.on('ready', () => {
-        client.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
-    
-    });
+p3.on('guildMemberAdd', member => {
+    p3.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
+
+});
 
 
 
-    console.log("oNNN4");
-    p4.on('ready', () => {
-        client.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
-    
-    });
+    console.log("oNNN3");
+p4.on('guildMemberAdd', member => {    
+    p4.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
+
+});
 
 
+    console.log("oNNN3");
+p5.on('guildMemberAdd', member => {
+    p5.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
 
-
-
-    console.log("oNNN5");
-    p5.on('ready', () => {
-        client.user.setGame(`Deluxe`,'https://www.twitch.tv/MeeRcY')
-    
-    });
-
-
-
-
-
+});
 
 
 
@@ -66,5 +59,6 @@ const p5 = new D5.Client();
 p1.login(process.env.B1);
 p2.login(process.env.B2);
 p3.login(process.env.B3);
-p4.login(process.env.B4);
+p5.login(process.env.B4);
 p5.login(process.env.B5);
+
